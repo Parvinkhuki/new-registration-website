ae6b4edf3e31fb9b11e35c73a711232699727966
Parvinkhuki <khuki.developer@gmail.com>
2023-09-15 03:23:28 +0600

    done

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,22 +1,23 @@
 
-const Cart = ({selectCourse}) => {
+const Cart = ({selectCourse,remainghrs, totalHrs ,totalcost}) => {
   
   
  
     return (
         <div className="w-1/3 mx-20 h-[500px] mt-10 sticky top-2  p-10 space-y-4 bg-white">
-           <h1 className="text-sky-400 font-bold">Credit hours remaining {20} hrs</h1> 
+           <h1 className="text-sky-400 font-bold">Credit hours remaining {remainghrs} hrs</h1> 
            <hr />
            <h1 className="font-bold text-lg">course Name</h1>
            {
-            selectCourse.map((course)=>(
+            selectCourse.map((course,i)=>(
                 <ol key={course.id} className="">
-                    <li>{course.title}</li>
+                    <li>{i+1}.{course.title}</li>
                 </ol>
             ))
            }
            <hr />
-           <h1>total cridit Hour: {0}</h1>
+           <h1><span className="font-bold">total Credit:</span> {totalHrs} hrs</h1>
+           <h1><span className="font-bold">total price:</span> {totalcost} $</h1>
 
         </div>
     );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,57 @@
 import Courses from "./Courses";
 import Cart from "./Cart";
 import { useState } from "react";
+import { toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
     const [selectCourse,setSelectCourse]=useState([])
-
+    const [totalHrs,setTotalhrs]=useState([0])
+    const [remainghrs,setremainghrs]=useState([20])
+    const [totalcost,setTotalcost]=useState([0])
+  
     const hendleSelect=(course)=>{
-      setSelectCourse([...selectCourse,course])
-        console.log(selectCourse)
+        let count=course.credit
+        let price=course.price
+        console.log(count)
+        const exist=selectCourse.find(newCourse=>newCourse.id===course.id)
+        if (exist) {
+          return  toast("it already have")
+        }
+     else{
+        selectCourse.forEach((newCourse)=>{
+          
+           count+=newCourse.credit
+           price+=newCourse.price
+        })
+        if(count>20){
+            return toast("you can only add under or equal 20 hrs ")
+        }
+        setTotalhrs(count)
+        const remaining= 20 - count
+        if(remaining<0){
+            return toast('remaining hrs cannot be 0 hrs')
+        }
+      
+      
+        console.log(totalHrs)
+        console.log(remaining)
+        setTotalcost(price)
+        setremainghrs(remaining)
+        setSelectCourse([...selectCourse,course])
+     }
+     
     }
     return (
         <div className="flex mx-7">
             <Courses 
     hendleSelect={hendleSelect}></Courses>
             <Cart 
-            selectCourse={selectCourse}></Cart>
+            selectCourse={selectCourse}
+            totalHrs={totalHrs}
+            remainghrs={remainghrs}
+            totalcost={totalcost}
+            ></Cart>
         </div>
     );
 };
